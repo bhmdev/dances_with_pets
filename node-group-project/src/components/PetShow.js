6203f4828d2b5105d5253d21549abfd51baf1ab2
bhmdev <bmontima@gmail.com>
2020-08-11 17:42:24 -0400

    These are my small spacing changes

--- node-group-project/src/components/PetShow.js
+++ node-group-project/src/components/PetShow.js
@@ -1,4 +1,5 @@
 import React from "react"
+
 const PetShow = props => {
   return (
     <div className="pet-show">
--- node-group-project/src/components/PetShowContainer.js
+++ node-group-project/src/components/PetShowContainer.js
@@ -1,34 +1,36 @@
 import React, { useEffect, useState } from "react"
 import PetShow from "./PetShow"
+
 const PetShowContainer = props => {
   const [pet, setPet] = useState(null);
+  
   useEffect(() => {
-    const petId = props.match.params.id   
+    const petId = props.match.params.id
     fetch(`/api/v1/pets/adopt/${petId}`)
-    .then(response => {
-      if (response.ok) {
-        return response;
-      } else {
-        let errorMessage = `${response.status} (${response.statusText})`,
+      .then(response => {
+        if (response.ok) {
+          return response;
+        } else {
+          let errorMessage = `${response.status} (${response.statusText})`,
             error = new Error(errorMessage);
-        throw(error);
-      }
-    })
-    .then(response => response.json())
-    .then(content => {
-      console.log('content.pet[0] = ', content.pet[0])
-      setPet(content.pet[0])
-    })
-    .catch(error => console.error(`Error in fetch: ${error.message}`));
+          throw (error);
+        }
+      })
+      .then(response => response.json())
+      .then(content => {
+        setPet(content.pet[0])
+      })
+      .catch(error => console.error(`Error in fetch: ${error.message}`));
   }, [])
+
   if (pet) {
     return (
       <PetShow
         name={pet.pet_name}
         thumbnail={pet.pet_thumbnail_url}
         vaccination={pet.pet_vaccination_status.toString()}
-        adoption={pet.pet_adoption_story} 
-        breed={pet.pet_type_id} 
+        adoption={pet.pet_adoption_story}
+        breed={pet.pet_type_id}
         ageYears={pet.pet_age.split('|')[0]}
         ageMonths={pet.pet_age.split('|')[1]}
       />
@@ -37,4 +39,5 @@ const PetShowContainer = props => {
     return <div>This is the pet show container</div>
   }
 }
+
 export default PetShowContainer
