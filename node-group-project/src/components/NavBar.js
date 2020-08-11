fa8b1ec5581bf9cd8338823f9ec1e9d03b22360f
Russ Moore <rmoore76@gmail.com>
2020-08-11 17:35:53 -0400

    Updated to indentation to match AirBNB Style Guide.

--- node-group-project/src/components/NavBar.js
+++ node-group-project/src/components/NavBar.js
@@ -3,18 +3,18 @@ import { Switch, Route, Link } from "react-router-dom"
 import PetTypeIndexContainer from "./PetTypeIndexContainer"
 
 const NavBar = props => {
-    return (
-        <div className="row column">
-            <div className="content">
-                <h1 className="page-title">Dances with Pets</h1>
-            </div>
-            <div className="navbar">
-                <Link to="/pets">All Pet Types</Link>
-            </div>
-            <Switch>
-                <Route exact path="/pets" component={PetTypeIndexContainer} />
-            </Switch>
-        </div>
-    )
+  return (
+    <div className="row column">
+      <div className="content">
+        <h1 className="page-title">Dances with Pets</h1>
+      </div>
+      <div className="navbar">
+        <Link to="/pets">All Pet Types</Link>
+      </div>
+      <Switch>
+        <Route exact path="/pets" component={PetTypeIndexContainer} />
+      </Switch>
+    </div>
+  )
 }
 export default NavBar
