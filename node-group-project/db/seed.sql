fe4d722c7cc0994a546c6c89ad8dd7ac27ac1f81
Tom Murphy <56618913+murphy-codes@users.noreply.github.com>
2020-08-11 08:01:04 -0700

    Added description to schema, thumbnails for pet types, and subfolders for public/img, as well updating the package.json to reflect our db name (import/seed).

--- node-group-project/db/seed.sql
+++ node-group-project/db/seed.sql
@@ -1,18 +1,25 @@
-INSERT INTO pet_types (pet_type_name, pet_category_name, pet_breed_name) VALUES ('cat', 'basic', 'domestic short hair');
-INSERT INTO pet_types (pet_type_name, pet_category_name, pet_breed_name) VALUES ('cat', 'basic', 'domestic medium hair');
-INSERT INTO pet_types (pet_type_name, pet_category_name, pet_breed_name) VALUES ('cat', 'basic', 'domestic long hair');
-INSERT INTO pet_types (pet_type_name, pet_category_name, pet_breed_name) VALUES ('dog', 'basic', 'German shepherd');
-INSERT INTO pet_types (pet_type_name, pet_category_name, pet_breed_name) VALUES ('dog', 'basic', 'maltipoo');
-INSERT INTO pet_types (pet_type_name, pet_category_name, pet_breed_name) VALUES ('tiger', 'exotic', 'Siberian');
-INSERT INTO pet_types (pet_type_name, pet_category_name, pet_breed_name) VALUES ('dragon', 'mythical', 'Slavic');
+INSERT INTO pet_types (pet_type_name, pet_type_thumbnail_url, pet_category_name, pet_breed_name, description)
+  VALUES ('cat', '/img/types/domestic-short-hair.jpg', 'basic', 'domestic short hair', 'A cat of mixed ancestry, thus not belonging to any particular recognized cat breed, possessing a coat of short fur.');
+INSERT INTO pet_types (pet_type_name, pet_type_thumbnail_url, pet_category_name, pet_breed_name, description)
+  VALUES ('cat', '/img/types/domestic-medium-hair.jpg', 'basic', 'domestic medium hair', 'A fluffy and docile cat, the domestic medium-hair is the perfect hybrid of his domestic short- and longhair relatives.');
+INSERT INTO pet_types (pet_type_name, pet_type_thumbnail_url, pet_category_name, pet_breed_name, description)
+  VALUES ('cat', '/img/types/domestic-long-hair', 'basic', 'domestic long hair', 'A cat of mixed ancestry, thus not belonging to any particular recognised cat breed, possessing a coat of semi-long to long fur.');
+INSERT INTO pet_types (pet_type_name, pet_type_thumbnail_url, pet_category_name, pet_breed_name, description)
+  VALUES ('dog', '/img/types/german-shepherd.jpg', 'basic', 'German Shepherd', 'The German Shepherd is a breed of medium to large-sized working dog that originated in Germany.');
+INSERT INTO pet_types (pet_type_name, pet_type_thumbnail_url, pet_category_name, pet_breed_name, description)
+  VALUES ('dog', '/img/types/maltipoo.jpg', 'basic', 'Maltipoo', 'The Maltipoo is a popular cross of the Maltese and Poodle, known for their fun-loving and affectionate nature.');
+INSERT INTO pet_types (pet_type_name, pet_type_thumbnail_url, pet_category_name, pet_breed_name, description)
+  VALUES ('tiger', '/img/types/siberian-tiger.jpg', 'exotic', 'Siberian', 'The Siberian tiger is a tiger from a specific population of the Panthera tigris tigris subspecies that is native to the Russian Far East, Northeast China, and possibly North Korea.');
+INSERT INTO pet_types (pet_type_name, pet_type_thumbnail_url, pet_category_name, pet_breed_name, description)
+  VALUES ('dragon', '/img/types/slavic-dragon.jpg', 'mythical', 'Slavic', 'A Slavic dragon is any dragon in Slavic mythology, including the Russian zmei, the Slovak drak and sarkan, Czech drak, Polish zmij, the Serbian and Croatian zmaj, the Macedonian zmey.');
 
 INSERT INTO pets (pet_name, pet_thumbnail_url, pet_age, pet_vaccination_status, pet_adoption_story, pet_adoption_status, pet_type_id)
-  VALUES ('Fiona', '/img/Fiona_01_450x600.png', '12|2', TRUE, E'She\'s a princess.', FALSE, 5);
+  VALUES ('Fiona', '/img/pets/Fiona_01_450x600.png', '12|2', TRUE, E'She\'s a princess.', FALSE, 5);
 INSERT INTO pets (pet_name, pet_thumbnail_url, pet_age, pet_vaccination_status, pet_adoption_story, pet_adoption_status, pet_type_id)
-  VALUES ('Midnight', '/img/Midnight_01_450x600.jpg', '20|0', TRUE, E'She\'s a queen.', FALSE, 1);
+  VALUES ('Midnight', '/img/pets/Midnight_01_450x600.jpg', '20|0', TRUE, E'She\'s a queen.', FALSE, 1);
 INSERT INTO pets (pet_name, pet_thumbnail_url, pet_age, pet_vaccination_status, pet_adoption_story, pet_adoption_status, pet_type_id)
-  VALUES ('Mimosa', '/img/Mimosa_01_450x600.jpg', '1|4', TRUE, E'She\'s a ferocious cuddler.', FALSE, 2);
+  VALUES ('Mimosa', '/img/pets/Mimosa_01_450x600.jpg', '1|4', TRUE, E'She\'s a ferocious cuddler.', FALSE, 2);
 INSERT INTO pets (pet_name, pet_thumbnail_url, pet_age, pet_vaccination_status, pet_adoption_story, pet_adoption_status, pet_type_id)
-  VALUES ('Merlot', '/img/Merlot_01_450x600.jpg', '2|3', TRUE, E'He\'s a little angel.', FALSE, 1);
+  VALUES ('Merlot', '/img/pets/Merlot_01_450x600.jpg', '2|3', TRUE, E'He\'s a little angel.', FALSE, 1);
 INSERT INTO pets (pet_name, pet_thumbnail_url, pet_age, pet_vaccination_status, pet_adoption_story, pet_adoption_status, pet_type_id)
-  VALUES ('Shadow', '/img/Shadow_01_450x600.jpg', '1|10', TRUE, E'He\'s handsome goofball.', FALSE, 4);
+  VALUES ('Shadow', '/img/pets/Shadow_01_450x600.jpg', '1|10', TRUE, E'He\'s handsome goofball.', FALSE, 4);
