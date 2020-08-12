96208876d228fc82f46def1bc200086a4ffab172
Tom Murphy <56618913+murphy-codes@users.noreply.github.com>
2020-08-12 09:52:47 -0700

    Small assisting functions

---
+++ node-group-project/src/functions/capFirstLetter.js
@@ -0,0 +1,11 @@
+import { capitalize } from "lodash"
+
+const capFirstLetter = (str) => {
+  const words = str.split(' ');
+  const capWords = words.map(word => {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  })
+  return capWords.join(' ');
+}
+
+export default capFirstLetter;
