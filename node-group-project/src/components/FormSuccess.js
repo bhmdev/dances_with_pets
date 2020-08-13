2282900bae454aee3b5f7bba5a75ee14e9fc1eb3
Tom Murphy <56618913+murphy-codes@users.noreply.github.com>
2020-08-13 14:55:26 -0700

    Indentation fixed

--- node-group-project/src/components/FormSuccess.js
+++ node-group-project/src/components/FormSuccess.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const FormSuccess = props => {
   return (
-  <h2 className="center-text margin-top-1">{props.formSuccessMessage}</h2>
+    <h2 className="center-text margin-top-1">{props.formSuccessMessage}</h2>
   )
 }
 
