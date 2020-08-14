d0d885af165fa24db326a5676600662996a6aa48
Tom Murphy <56618913+murphy-codes@users.noreply.github.com>
2020-08-14 08:55:22 -0700

    updated PetIndexContainer to render various types of pets & lists of pets

--- node-group-project/src/components/PetIndexContainer.js
+++ node-group-project/src/components/PetIndexContainer.js
@@ -1,15 +1,29 @@
 import React, { useState, useEffect } from "react"
 import PetTile from "./PetTile"
 import getPath from "../functions/getPath.js"
+import capFirstLetter from "../functions/capFirstLetter.js"
 
 const PetTypeIndexContainer = props => {
   const [pets, setPets] = useState([])
+  let petsListed, path;
+  
+  path = getPath()
+  petsListed = (path.length>1 ? path[1] : path[0]);
+  if (petsListed === `past-pets`) {petsListed = `past pets`}
+  else if (path[0] === `classification`) {
+    if (path[1] === `domesticated`) { petsListed = `domestic`}
+    petsListed += ` pets`
+  } else if (path.length>1) {
+    while (petsListed.includes('%20')) {
+      petsListed = petsListed.replace('%20', ' ');
+    }
+    if (petsListed.charAt(petsListed.length-1) === `s`) { petsListed += `es`; }
+    else { petsListed += `s`; };
+  }
 
   useEffect(() => {
-    let path = getPath()
-    let fetchString = `/api/v1/`;
-
-    fetch('/api/v1/pets')
+    let fetchString = `/api/v1/${path[0]}${path.length>1 ? `/${path[1]}` : ``}`;
+    fetch(fetchString)
       .then(response => {
         if (response.ok) {
           return response;
@@ -24,40 +38,27 @@ const PetTypeIndexContainer = props => {
         setPets(body.pets);
       })
       .catch(error => console.error(`Error in fetch: ${error.message}`));
-  },[])
-
-  // const petTypeTiles = pets.map(petType => {
-  //   return (
-  //     <PetTypeTile
-  //       key={petType.id}
-  //       name={petType.pet_type_name}
-  //       thumbnail={petType.pet_type_thumbnail_url}
-  //       category={petType.pet_category_name}
-  //       breed={petType.pet_breed_name}
-  //       description={petType.description}
-  //     />
-  //   )
-  // })
+  },[path])
 
   const petTiles = pets.map(pet => {
     return (
       <PetTile
-        key={pet.id}
-        name={pet.pet_type_name}
-        thumbnail={pet.pet_type_thumbnail_url}
-        category={pet.pet_category_name}
-        breed={pet.pet_breed_name}
-        description={pet.description}
+      key={pet.id}
+      name={pet.name}
+      breed={capFirstLetter(pet.breed)}
+      age={pet.age}
+      description={pet.description}
+      thumbnail={pet.pet_img_url}
       />
     )
   })
 
   return (
-      <div className="row">
+    <div className="row">
       <div className="small-8 small-centered columns">
-        <h1>View our pet types!</h1>
+        <h1 className="center-text">View our {petsListed}!</h1>
         <hr />
-        {petTypeTiles}
+        {petTiles }
       </div>
     </div>
   )
