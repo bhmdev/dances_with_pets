61ede0da502232b7553e9d4eb12405faa40df87e
Tom Murphy <56618913+murphy-codes@users.noreply.github.com>
2020-08-14 08:48:38 -0700

    updated grouping components to render various types of groupings

--- node-group-project/src/components/GroupingTile.js
+++ node-group-project/src/components/GroupingTile.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { Link } from "react-router-dom"
-
 import capFirstLetter from "../functions/capFirstLetter.js"
 
 const GroupingTile = props => {
@@ -9,7 +8,7 @@ const GroupingTile = props => {
   
   return (
     <div className="pet-type-tile">
-      <p className="center-text"><Link to={`/pets/${props.group}`}>{group}</Link></p>
+      <p className="center-text"><Link to={`/${props.grouping}/${props.group}`}>{group}</Link></p>
       <hr />
     </div>
   )
