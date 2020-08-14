abef6e8658ba451ee700bc0102153070bf5811ef
Tom Murphy <56618913+murphy-codes@users.noreply.github.com>
2020-08-13 20:30:47 -0700

    updated age constraints on TABLE pets

--- node-group-project/db/schema.sql
+++ node-group-project/db/schema.sql
@@ -16,7 +16,7 @@ CREATE TABLE pets (
   id SERIAL PRIMARY KEY,
   name VARCHAR(50) NOT NULL UNIQUE,
   pet_img_url VARCHAR(255) NOT NULL,
-  age VARCHAR(20),
+  age VARCHAR(12),
   vaccination_status BOOLEAN,
   adoption_story TEXT NOT NULL,
   adoption_status BOOLEAN NOT NULL,
